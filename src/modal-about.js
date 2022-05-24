540afbcfaf3daf24a9f9c7a9335b577c741cf1d2
Andrzej Markiewicz <endercom502@gmail.com>
2022-05-24 21:23:17 +0200

    21:23

--- src/modal-about.js
+++ src/modal-about.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('.start-modal'),
-    closeModalBtn: document.querySelector('.end-modal'),
-    modal: document.querySelector('.content-modal'),
+    openModalBtn: document.querySelector('[start-modal]'),
+    closeModalBtn: document.querySelector('[end-modal]'),
+    modal: document.querySelector('[content-modal]'),
   };
   
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-hiddenn');
   }
 })();
