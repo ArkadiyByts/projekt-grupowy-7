e3ad19423177718add0684697074d36053557f1c
Andrzej Markiewicz <endercom502@gmail.com>
2022-05-24 19:07:35 +0200

    19:6

---
+++ src/modal-about.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.start-modal-about'),
+    closeModalBtn: document.querySelector('.end-modal-about'),
+    modal: document.querySelector('.content-modal-about'),
+  };
+  
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
